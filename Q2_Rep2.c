46d10a9cd5b4594732d00d539826ec550cc58967
SK Upadhyay <saurabhupadhyay318@gmail.com>
2018-04-14 03:40:49 +0530

    Update Q2_Rep2.c

--- Q2_Rep2.c
+++ Q2_Rep2.c
@@ -1,11 +1,3 @@
-
-/*
-	Preemptive Priority
-		By:-
-			SK Upadhyay
-*/
-
-
 #include<stdio.h>
 struct data
 {
@@ -14,8 +6,11 @@ struct data
 	int arrival;
 	int priority;
 }d[100];
-struct dataprocess
+
+struct processdat
 {
+    int cburst;
+    int rank;
     int burst;
     int status;
     int wait;
@@ -28,7 +23,7 @@ int shortBurst(int, int);
 
 main()
 {
-	int i,j,n,swapburst, swapname, swaparrival;
+	int i,j,n,swapburst;
 	printf("\n\t> Preemptive Priority Scheduler<");
 	printf("\n\t> Data Input");
 	printf("\n\t> Enter Total No. of Process:\t");
@@ -41,38 +36,86 @@ main()
 		printf("\n\t> Enter Burst Time:\t");
 		scanf("%d", &d[i].burst);
 		d[i].name=i+1;
-		//printf("\n\t> Enter Priority:\t");
-		//scanf("%d", &d[i].priority);
+		p[i].cburst=d[i].burst;
+		p[i].burst=d[i].burst;
 
 	}
 	for(i=0;i<n;i++)							        //Shorting Process
-		{
-			for(j=i;j<n;j++)
-			{
-				if(d[i].burst>d[j].burst)
-				{
-					swapburst=d[i].burst;
-					d[i].burst=d[j].burst;
-					d[j].burst=swapburst;
-					swapname=d[i].name;
-					d[i].name=d[j].name;
-					d[j].name=swapname;
-                    swaparrival=d[i].arrival;
-                    d[i].arrival=d[j].arrival;
-                    d[j].arrival=swaparrival;
-				}
-			}
-		}
-    for(i=0;i<n;i++)						
     {
-        d[i].priority =i+1;
+        for(j=i;j<n;j++)
+        {
+            if(p[i].cburst>p[j].cburst)
+            {
+                swapburst=p[i].cburst;
+                p[i].cburst=p[j].cburst;
+                p[j].cburst=swapburst;
+            }
+        }
+    }
+    for(i=0;i<n;i++)
+    {
+        p[i].rank=i+1;
         p[i].status=0;
         p[i].wait=0;
-        p[i].burst=d[i].burst;
     }
+    for(i=0;i<n;i++)
+    {
+        for(j=0;j<n;j++)
+        {
+            if(p[j].cburst==p[i].cburst)
+            {
+                p[j].rank=p[i].rank;
+            }
+        }
+    }
+    for(i=0;i<n;i++)
+	{
+		for(j=0;j<n;j++)
+		{
+			if(d[i].burst==p[j].cburst)
+			{
+				d[i].priority=p[j].rank;
+			}
+		}
+	}
 	system("cls");
 	print(n);
+
+	int totalBurst,low,pr;
+        totalBurst=Burst(n);
+	  for (i=0;i<totalBurst;i++)
+	   {
+		low=shortBurst(n,i);
+		 p[low].burst=p[low].burst - 1;
+		 for (j=0;j<n;j++)
+	  	{
+		 if( (low!=j) && (d[j].arrival<=i) && (p[j].status!=1) )
+		   {
+			 p[j].wait=p[j].wait+1;
+		   }
+        }
+		 if(p[low].burst==0)
+		  {
+			p[low].status=1;
+		  }
+	   }
+	   printnow(n);
+    float sum=0.0,sum2=0.0;
+	 float ave, tat;
+
+	 for (i=0;i<n;i++)
+	  {
+		sum=sum+p[i].wait;
+		sum2+=p[i].wait+d[i].burst;
+	  }
+	 ave = sum/n;
+	 tat=sum2/n;
+	 printf("\n\n\n \t Average Waiting Time : %0.2f",ave);
+	 printf("\n\n\n \t Average TurnAround Time : %0.2f",tat);
+
+
 }
+
 void print(int n)
 {
 	int i;
@@ -85,3 +128,40 @@ void print(int n)
 	}
 	printf("\n\t>-----------------------------------------------------------------------<");
 }
+
+void printnow(int n)
+{
+	int i;
+	printf("\n\t\t\t\t-> Stored Data <-");
+	printf("\n\t>-----------------------------------------------------------------------<");
+	printf("\n\t|\tPROCESS\t\tARRIVAL\t\tBURST\t\tPRIORITY\t\tWAIT\t|");
+	for(i=0;i<n;i++)
+	{
+		printf("\n\t|\t  P%d\t\t   %d\t\t  %d\t\t   %d\t\t   %d\t\t|",d[i].name,d[i].arrival,d[i].burst,d[i].priority,p[i].wait);
+	}
+	printf("\n\t>-----------------------------------------------------------------------<");
+}
+int Burst(int n)
+{
+    int i,totalBurst=0;
+    for (i=0;i<n;i++)
+    {
+        totalBurst=totalBurst+d[i].burst;
+    }
+    return totalBurst;
+}
+int shortBurst(int n,int excutedtime)
+{
+    int i,min,pr;
+    min=10;
+    for (i=0;i<n;i++)
+    {
+		if( (p[i].status!=1) && (d[i].priority<min) && (d[i].arrival<=excutedtime))
+	 	{
+			min=p[i].burst;
+			pr=i;
+	 	}
+    }
+    return pr;
+}
+
