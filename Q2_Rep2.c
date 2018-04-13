f6ea763ff20205fe445fb4e077be8eee2a02c9d1
SK Upadhyay <saurabhupadhyay318@gmail.com>
2018-04-14 02:06:32 +0530

    Update Q2_Rep2.c

--- Q2_Rep2.c
+++ Q2_Rep2.c
@@ -14,12 +14,21 @@ struct data
 	int arrival;
 	int priority;
 }d[100];
+struct dataprocess
+{
+    int burst;
+    int status;
+    int wait;
+}p[100];
 
+void printnow(int);
 void print(int);
+int Burst(int);
+int shortBurst(int, int);
 
 main()
 {
-	int i,n;
+	int i,j,n,swapburst, swapname, swaparrival;
 	printf("\n\t> Preemptive Priority Scheduler<");
 	printf("\n\t> Data Input");
 	printf("\n\t> Enter Total No. of Process:\t");
@@ -31,9 +40,36 @@ main()
 		scanf("%d", &d[i].arrival);
 		printf("\n\t> Enter Burst Time:\t");
 		scanf("%d", &d[i].burst);
-		printf("\n\t> Enter Priority:\t");
-		scanf("%d", &d[i].priority);
+		d[i].name=i+1;
+		//printf("\n\t> Enter Priority:\t");
+		//scanf("%d", &d[i].priority);
+
 	}
+	for(i=0;i<n;i++)							        //Shorting Process
+		{
+			for(j=i;j<n;j++)
+			{
+				if(d[i].burst>d[j].burst)
+				{
+					swapburst=d[i].burst;
+					d[i].burst=d[j].burst;
+					d[j].burst=swapburst;
+					swapname=d[i].name;
+					d[i].name=d[j].name;
+					d[j].name=swapname;
+                    swaparrival=d[i].arrival;
+                    d[i].arrival=d[j].arrival;
+                    d[j].arrival=swaparrival;
+				}
+			}
+		}
+    for(i=0;i<n;i++)						
+    {
+        d[i].priority =i+1;
+        p[i].status=0;
+        p[i].wait=0;
+        p[i].burst=d[i].burst;
+    }
 	system("cls");
 	print(n);
 }
